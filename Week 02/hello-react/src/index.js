bd7ec9e0f6df408d811d20f830cd7c491fbc53a3
scarlwang <mw4016@nyu.edu>
2025-11-10 13:18:24 -0500

    Add Week 02

---
+++ Week 02/hello-react/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root')
+
+const root = ReactDOM.createRoot(el)
+
+function App() {
+  return <h1>Hello World!</h1>
+}
+
+root.render(<App />)
