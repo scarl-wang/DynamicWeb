29b806fcc3d058eaa377af6b9405d1b15af50ece
scarlwang <mw4016@nyu.edu>
2025-11-10 15:29:38 -0500

    Add Week 05

---
+++ Week 05/comp-lib-routes/src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import ButtonPage from "./pages/ButtonPage";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+
+const App = () => {
+  return (
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Navbar />
+      <div className="col-span-5">
+        <Routes>
+          <Route path="/" element={<ButtonPage />}></Route>
+          <Route path="/accordion" element={<AccordionPage />}></Route>
+          <Route path="/dropdown" element={<DropdownPage />}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
